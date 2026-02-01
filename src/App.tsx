8a68b3323ef6cc06bf4cd2bdb117b4ad6eb33e27
lamchiwing <60570944+lamchiwing@users.noreply.github.com>
2026-02-01 10:44:48 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import Header from './components/Header';
+import Header from "./components/Header";
 import Hero from './components/Hero';
 import ToolSection from './components/ToolSection';
 import AIConsultant from './components/AIConsultant';
